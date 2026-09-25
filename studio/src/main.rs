65bcc037fe3e7b6e2f62f0c0ae353b014aaa7b68
Sam Hill <soshjam@soshjam.com>
2026-09-25 09:17:54 -0600

    Ensure compute pipeline availability

--- studio/src/main.rs
+++ studio/src/main.rs
@@ -1,14 +1,33 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
+use std::sync::Arc;
+
+use eframe::{egui};
+use eframe::egui_wgpu::{wgpu, WgpuConfiguration, WgpuSetup, WgpuSetupCreateNew};
 use studio::app::StudioApp;
 
 fn main() -> eframe::Result {
-    // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-    // Set up the viewport
+    // Set up WGPU
+    let mut setup = WgpuSetupCreateNew::without_display_handle();
+    setup.device_descriptor = Arc::new(|adapter| {
+        let base = wgpu::Limits::downlevel_defaults();
+        wgpu::DeviceDescriptor {
+            label: Some("egui+compute device"),
+            required_features: wgpu::Features::empty(),
+            required_limits: base.using_resolution(adapter.limits()),
+            ..Default::default()
+        }
+    });
+
+    // Set up the viewport for WGPU
     let options = eframe::NativeOptions {
         renderer: eframe::Renderer::Wgpu,
+        wgpu_options: WgpuConfiguration {
+            wgpu_setup: WgpuSetup::CreateNew(setup),
+            ..Default::default()
+        },
         viewport: egui::ViewportBuilder::default().with_inner_size([960.0, 540.0]),
         ..Default::default()
     };
