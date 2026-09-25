da35b74996f89bc72498faafa54f03affd33620d
Sam Hill <soshjam@soshjam.com>
2026-09-25 09:32:06 -0600

    Shrink rendering crate

--- rendering/src/viewport.rs
+++ rendering/src/viewport.rs
@@ -1,15 +1,14 @@
 use wgpu::{Device, util::DeviceExt};
 
-
 // Needed for Rust to store data correctly for shaders
 #[repr(C)]
 // Needed for storing into a buffer
 #[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-pub struct CameraUniform {
+pub struct ViewportDataUniform {
     blue: u32, // we just need something to pass to the buffer
 }
 
-impl CameraUniform {
+impl ViewportDataUniform {
     pub fn new(blue: u32) -> Self {
         Self {
             blue,
@@ -18,12 +17,12 @@ impl CameraUniform {
 
     pub fn buffer(device: &Device) -> wgpu::Buffer {
         // Create initial contents
-        let camera = Self::new(0);
+        let data = Self::new(0);
 
         // Create and return the buffer
         device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Camera Buffer"),
-            contents: bytemuck::cast_slice(&[camera]),
+            contents: bytemuck::cast_slice(&[data]),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         })
     }
